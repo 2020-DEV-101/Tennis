b5cdae7976982440d519c4d5e1708fe560291248
2020-DEV-101 <noreply@github.com>
2020-07-16 21:08:18 +0200

    Refactored tests

--- src/test/java/be/kata/tennis/PlayerTests.kt
+++ src/test/java/be/kata/tennis/PlayerTests.kt
@@ -4,9 +4,8 @@ import org.hamcrest.CoreMatchers.equalTo
 import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.Matchers.`is`
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertThrows
 
-class TennisTests {
+class PlayerTests {
 
     @Test
     fun `A player should start at 0 points`() {
@@ -45,22 +44,6 @@ class TennisTests {
 
         repeat(4) { playerOne.scorePoint() }
         assertThat(playerOne.hasWon, `is`(true))
-    }
-
-    @Test
-    fun `When both players of a game have 40 points, they are at deuce`() {
-        val playerOne = Player()
-        val playerTwo = Player()
-        val game = TennisGame(playerOne, playerTwo)
-
-        repeat(3) { game.playerOneScores() }
-        repeat(3) { game.playerTwoScores() }
-        assertThat(playerOne.score, `is`(equalTo(40)))
-        assertThat(playerTwo.score, `is`(equalTo(40)))
-        assertThat(playerOne.isDeuce, `is`(true))
-        assertThat(playerTwo.isDeuce, `is`(true))
-        assertThat(game.isDeuce, `is`(true))
-
     }
 
 }
