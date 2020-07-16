b249d5fa73cfe739e58921821a0843f691fcd780
2020-DEV-101 <noreply@github.com>
2020-07-16 19:18:49 +0200

    Added main function and useless test to see if it runs

---
+++ src/main/java/be/kata/tennis/Main.kt
@@ -0,0 +1,6 @@
+package be.kata.tennis
+
+fun main(args: Array<String>) {
+    println("Ping")
+    println("Pong")
+}
---
+++ src/test/java/be/kata/tennis/TennisTests.kt
@@ -0,0 +1,13 @@
+package be.kata.tennis
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class TennisTests {
+
+@Test
+fun simpleTest(){
+    assertEquals("one", "one")
+}
+
+}
