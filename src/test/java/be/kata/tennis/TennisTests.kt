ecb893750d859c37b1c3e362c1912a2c98f077e8
2020-DEV-101 <noreply@github.com>
2020-07-16 19:40:05 +0200

    Added test: A player should start at 0 points In TDD, the test is added before the functionality. The test is currently commented out as it won't compile yet. Normally the test and functionality would be added in a single commit.

--- src/test/java/be/kata/tennis/TennisTests.kt
+++ src/test/java/be/kata/tennis/TennisTests.kt
@@ -1,13 +1,21 @@
 package be.kata.tennis
 
+import org.hamcrest.CoreMatchers.equalTo
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.`is`
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
 
 class TennisTests {
 
+@Disabled
 @Test
-fun simpleTest(){
-    assertEquals("one", "one")
+fun `A player should start at 0 points`(){
+//    val playerOne = Player()
+//
+//    assertEquals(playerOne.score, 0)
+//    assertThat(playerOne.score, `is`(equalTo(0)))
 }
 
 }
